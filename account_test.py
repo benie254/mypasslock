4f1cb48f11cbc741b6500ed0c49a42f81064b33b
Benie <davinci.monalissa@gmail.com>
2022-04-25 20:13:32 +0300

    Add a setUp method to initialize tests.

--- account_test.py
+++ account_test.py
@@ -1,5 +1,5 @@
 import unittest #unittest module
-from account import Account
+from account import Account #account class
 
 
 class TestAccount(unittest.TestCase):
@@ -7,3 +7,11 @@ class TestAccount(unittest.TestCase):
     '''
     To define test cases for the pass account's behaviors
     '''
+    
+    def setUp(self):
+        
+        '''
+        The Set up method--required at the initialization of tests
+        '''
+        
+        self.new_account = Account('Twitter', 'benie@gmail.com', '0708646667', 'benie','mypassword')
