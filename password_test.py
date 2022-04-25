66abb6d386fb3bfc8770aa78c72e7b7f2032d685
Benie <davinci.monalissa@gmail.com>
2022-04-25 21:38:24 +0300

    import unittest module; import password class; create TestPassword class.

---
+++ password_test.py
@@ -0,0 +1,9 @@
+import unittest #unittest module
+from password import Password #password class
+
+
+class TestPassword(unittest.TestCase):
+    
+    '''
+    To define test cases for the account password's behaviors
+    '''
