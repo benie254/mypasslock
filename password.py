225f45cec1c5a2633cf360e97a40a782863cfdd7
Benie <davinci.monalissa@gmail.com>
2022-04-25 21:29:21 +0300

    Add a method that finds password.

--- password.py
+++ password.py
@@ -28,3 +28,15 @@ class Password:
         
         self.lock_user = lock_user
         self.lock_pass = lock_pass
+        
+        
+    @classmethod
+    def find_pass_by_id(cls,id):
+        
+        '''
+        Method--takes in password input and finds it from the credentials list
+        '''
+        
+        for password in cls.login_details:
+            if password.lock_pass == id:
+                return password
