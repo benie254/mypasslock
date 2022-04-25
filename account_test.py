10c68702b4e816b3671e98ab55547490f34ba87a
Benie <davinci.monalissa@gmail.com>
2022-04-25 20:11:11 +0300

    import unittest module; import Account class; create a test class.

---
+++ account_test.py
@@ -0,0 +1,9 @@
+import unittest #unittest module
+from account import Account
+
+
+class TestAccount(unittest.TestCase):
+    
+    '''
+    To define test cases for the pass account's behaviors
+    '''
