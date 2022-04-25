0d558ec7528c5b5557cbb1481e8654f865eb72c9
Benie <davinci.monalissa@gmail.com>
2022-04-25 19:55:58 +0300

    Add an __init__ method--to define properties of account objects.

--- account.py
+++ account.py
@@ -5,3 +5,11 @@ class Account:
     """
     
     account_details = [] #empty account details list
+    
+    def __init__(self, account, email, phone_number, username, password):
+        
+        self.account = account
+        self.email = email
+        self.phone_number = phone_number
+        self.username = username
+        self.password = password
