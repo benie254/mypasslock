376cad7dfd32e16b31d3020ee6e87211451e3cbf
Benie <davinci.monalissa@gmail.com>
2022-04-25 21:27:29 +0300

    Add an __init__ method to define properties of login credentials.

--- password.py
+++ password.py
@@ -18,3 +18,13 @@ class Password:
         '''
         
         Password.login_details.append(self)
+        
+        
+    def __init__(self,lock_user,lock_pass):
+        
+        '''
+        To define properties of login credentials objects
+        '''
+        
+        self.lock_user = lock_user
+        self.lock_pass = lock_pass
