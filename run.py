217186dd5610bcbbfbf526b4eac1ea81a55a5c5c
Benie <davinci.monalissa@gmail.com>
2022-04-25 22:27:21 +0300

    Add methods to Create account, Add existing account, Create account password, and save new accounts.

--- run.py
+++ run.py
@@ -4,3 +4,30 @@ from account import Account
 from password import Password
 
 account_details = []
+
+
+#create an account
+def create_account(acc,email,phone,user,password):
+
+    new_account = Account(acc,email,phone,user,password)
+    return new_account
+
+
+#add an existing account
+def existing_account(ex_acc,ex_email,ex_phone,ex_user,ex_password):
+
+    existing_account = Account(ex_acc,ex_email,ex_phone,ex_user,ex_password)
+    return existing_account
+
+
+#create the account login password
+def create_account_password(credent_user,credent_pass):
+    
+    new_pass = Password(credent_user,credent_pass)
+    return new_pass
+
+
+#save new accounts
+def save_new_accounts(account):
+
+    account.save_new_account()
